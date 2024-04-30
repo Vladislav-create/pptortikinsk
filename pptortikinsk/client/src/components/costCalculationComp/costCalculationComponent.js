c6d8a8b75eee863cdad0ea347548f73aa6b20296
Vlad <29-vinitskiy-vo@mail.ru>
2024-04-30 23:41:01 +0700

    Добавление сервера. Начало

--- pptortikinsk/client/src/components/costCalculationComp/costCalculationComponent.js
+++ pptortikinsk/client/src/components/costCalculationComp/costCalculationComponent.js
@@ -1,4 +1,14 @@
+// import { response } from "express";
+import { useEffect } from "react"
+
 export function CostCalculationComponent() {
+    
+    useEffect(() => {
+        fetch('/api/bricks').then(response => response.json()).then(data => {
+            console.log(data);
+        })
+        console.log("CostCalculationComponent");
+    }, []);
 
     return (
     
--- pptortikinsk/server.js
+++ pptortikinsk/server.js
@@ -3,15 +3,17 @@ const express = require("express");
 
 const app = express();
 
-// const PORT = process.env.PORT;
-// const HOSTNAME = process.env.HOSTNAME;
+const arr = [
+    {id: 1, text: 'asdasdasdas'}
+];
 
-// app.use(express.json());
+app.get('/api/bricks', (req, res) => {
+    console.log('Get all bricks');
+    res.json(arr);
+})
 
 app.listen(5000, () => {
     console.log("Сервер запущен.");
 });
 
-// app.listen(PORT, HOSTNAME, () => {
-//     console.log('Сервер запущен');
-// })
+
