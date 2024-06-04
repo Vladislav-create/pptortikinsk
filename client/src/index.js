8859a2aba2d4a67d09094d3fb54541f87380436f
Vlad <29-vinitskiy-vo@mail.ru>
2024-06-04 23:15:11 +0700

    fixed err

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import styles from "./components/global.module.css"
-// import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 // import { TestComp } from "./components/testComp/testComponent";
